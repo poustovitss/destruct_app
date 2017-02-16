0cde95fa67e276758b907751305ac1762672ecdb
Sergiy Poustovit <levellevel4@gmail.com>
2017-02-16 15:52:38 +0200

    deleted console logs

--- public/js/decryption.js
+++ public/js/decryption.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
   $('#decrypt').on('click', function () {
-    console.log('clicked');
+
     var password = $('#password').val();
     var decrypted = CryptoJS.AES.decrypt(window.encrypted_message, password);
-    console.log(password);
-    console.log(window.encrypted_message);
-    console.log(decrypted.toString());
+
     $('#decrypted_content').text(decrypted.toString(CryptoJS.enc.Utf8));
   });
 });
--- public/js/encryption.js
+++ public/js/encryption.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
     if ($('#has_password').prop('checked')) {
       var password = $('#password').val();
       message = CryptoJS.AES.encrypt(message, password);
-      console.log(message);
     }
 
     $('#encrypted_message').val(message);
